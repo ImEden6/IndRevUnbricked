c188cedd9049d0bed05975a617a3737f3c4ad71d
ImEden6 <mervyn.h.mh@gmail.com>
2026-06-21 12:51:57 +0800

    Remove dashloader-sources from repo tracking and update gitignore

--- dashloader-sources/dev/notalpha/dashloader/api/registry/RegistryReader.java
+++
@@ -1,9 +0,0 @@
-/*
- * Decompiled with CFR 0.152.
- */
-package dev.notalpha.dashloader.api.registry;
-
-public interface RegistryReader {
-    public <R> R get(int var1);
-}
-
--- dashloader-sources/dev/notalpha/dashloader/api/registry/RegistryWriter.java
+++
@@ -1,9 +0,0 @@
-/*
- * Decompiled with CFR 0.152.
- */
-package dev.notalpha.dashloader.api.registry;
-
-public interface RegistryWriter {
-    public <R> int add(R var1);
-}
-
