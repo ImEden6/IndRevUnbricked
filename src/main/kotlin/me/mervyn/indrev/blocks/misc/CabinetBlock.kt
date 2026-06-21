a4871301fd2ddf9088d4edb6448ad4ba370fbb98
ImEden6 <mervyn.h.mh@gmail.com>
2026-06-22 00:07:22 +0800

    container fix, staying on 1.0.3

--- src/main/kotlin/me/mervyn/indrev/blocks/misc/CabinetBlock.kt
+++ src/main/kotlin/me/mervyn/indrev/blocks/misc/CabinetBlock.kt
@@ -3,10 +3,12 @@ package me.mervyn.indrev.blocks.misc
 import me.mervyn.indrev.blockentities.storage.CabinetBlockEntity
 import net.minecraft.block.BlockEntityProvider
 import net.minecraft.block.BlockState
+import net.minecraft.block.HorizontalFacingBlock
 import net.minecraft.block.entity.BlockEntity
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.util.ActionResult
 import net.minecraft.util.Hand
+import net.minecraft.util.ItemScatterer
 import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
@@ -26,5 +28,22 @@ class CabinetBlock(settings: Settings) : HorizontalFacingBlock(settings), BlockE
             player.openHandledScreen(blockEntity)
         }
         return ActionResult.success(world.isClient)
+    }
+
+    override fun onStateReplaced(
+        state: BlockState,
+        world: World,
+        pos: BlockPos,
+        newState: BlockState,
+        moved: Boolean
+    ) {
+        if (state.block != newState.block) {
+            val blockEntity = world.getBlockEntity(pos) as? CabinetBlockEntity
+            if (blockEntity != null) {
+                ItemScatterer.spawn(world, pos, blockEntity)
+                world.updateComparators(pos, this)
+            }
+            super.onStateReplaced(state, world, pos, newState, moved)
+        }
     }
 }
