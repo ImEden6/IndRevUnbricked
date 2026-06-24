e1a3b03432cd83e1ebf17d79aa653bb7065c6c13
ImEden6 <mervyn.h.mh@gmail.com>
2026-06-24 23:29:28 +0800

    1.0.14 patch bump, emo plugin

--- src/main/kotlin/me/mervyn/indrev_unbricked/compat/emi/EMIPlugin.kt
+++ src/main/kotlin/me/mervyn/indrev_unbricked/compat/emi/EMIPlugin.kt
@@ -3,7 +3,6 @@ package me.mervyn.indrev_unbricked.compat.emi
 import dev.emi.emi.api.EmiPlugin
 import dev.emi.emi.api.EmiRegistry
 import dev.emi.emi.api.recipe.EmiInfoRecipe
-import dev.emi.emi.api.recipe.EmiRecipe
 import dev.emi.emi.api.recipe.EmiRecipeCategory
 import dev.emi.emi.api.stack.EmiIngredient
 import dev.emi.emi.api.stack.EmiStack
@@ -23,69 +22,55 @@ object EMIPlugin : EmiPlugin {
 
     private val categories = mutableMapOf<Identifier, EmiRecipeCategory>()
 
-    private val recipeMachineMap = mapOf(
-        PulverizerRecipe.IDENTIFIER to MachineRegistry.PULVERIZER_REGISTRY,
-        InfuserRecipe.IDENTIFIER to MachineRegistry.SOLID_INFUSER_REGISTRY,
-        CompressorRecipe.IDENTIFIER to MachineRegistry.COMPRESSOR_REGISTRY,
-        RecyclerRecipe.IDENTIFIER to MachineRegistry.RECYCLER_REGISTRY,
-        FluidInfuserRecipe.IDENTIFIER to MachineRegistry.FLUID_INFUSER_REGISTRY,
-        CondenserRecipe.IDENTIFIER to MachineRegistry.CONDENSER_REGISTRY,
-        SmelterRecipe.IDENTIFIER to MachineRegistry.SMELTER_REGISTRY,
-        SawmillRecipe.IDENTIFIER to MachineRegistry.SAWMILL_REGISTRY,
-        ModuleRecipe.IDENTIFIER to MachineRegistry.MODULAR_WORKBENCH_REGISTRY,
-        LaserRecipe.IDENTIFIER to MachineRegistry.LASER_EMITTER_REGISTRY,
-        ElectrolysisRecipe.IDENTIFIER to MachineRegistry.ELECTROLYTIC_SEPARATOR_REGISTRY,
+    private data class Entry<T : IRRecipe>(
+        val type: IRRecipeType<T>,
+        val registry: MachineRegistry
+    )
+
+    private val entries: List<Entry<*>> = listOf(
+        Entry(PulverizerRecipe.TYPE, MachineRegistry.PULVERIZER_REGISTRY),
+        Entry(InfuserRecipe.TYPE, MachineRegistry.SOLID_INFUSER_REGISTRY),
+        Entry(CompressorRecipe.TYPE, MachineRegistry.COMPRESSOR_REGISTRY),
+        Entry(RecyclerRecipe.TYPE, MachineRegistry.RECYCLER_REGISTRY),
+        Entry(FluidInfuserRecipe.TYPE, MachineRegistry.FLUID_INFUSER_REGISTRY),
+        Entry(CondenserRecipe.TYPE, MachineRegistry.CONDENSER_REGISTRY),
+        Entry(SmelterRecipe.TYPE, MachineRegistry.SMELTER_REGISTRY),
+        Entry(SawmillRecipe.TYPE, MachineRegistry.SAWMILL_REGISTRY),
+        Entry(ModuleRecipe.TYPE, MachineRegistry.MODULAR_WORKBENCH_REGISTRY),
+        Entry(LaserRecipe.TYPE, MachineRegistry.LASER_EMITTER_REGISTRY),
+        Entry(ElectrolysisRecipe.TYPE, MachineRegistry.ELECTROLYTIC_SEPARATOR_REGISTRY),
     )
 
     override fun register(registry: EmiRegistry) {
         registerCategories(registry)
         registerRecipes(registry)
-        registerWorkstations(registry)
         registerUpgradeInfo(registry)
         hideItems(registry)
     }
 
     private fun registerCategories(registry: EmiRegistry) {
-        fun add(id: Identifier, icon: ItemStack) {
-            val catId = Identifier(IndustrialRevolution.MOD_ID, id.path)
-            val category = EmiRecipeCategory(catId, EmiStack.of(icon))
-            categories[id] = category
+        for (entry in entries) {
+            val id = Identifier(IndustrialRevolution.MOD_ID, entry.type.id.path)
+            val icon = EmiStack.of(entry.registry.block(entry.registry.tiers[0]))
+            val category = EmiRecipeCategory(id, icon)
+            categories[entry.type.id] = category
             registry.addCategory(category)
-        }
 
-        add(PulverizerRecipe.IDENTIFIER, ItemStack(MachineRegistry.PULVERIZER_REGISTRY.block(Tier.MK1)))
-        add(InfuserRecipe.IDENTIFIER, ItemStack(MachineRegistry.SOLID_INFUSER_REGISTRY.block(Tier.MK1)))
-        add(CompressorRecipe.IDENTIFIER, ItemStack(MachineRegistry.COMPRESSOR_REGISTRY.block(Tier.MK1)))
-        add(RecyclerRecipe.IDENTIFIER, ItemStack(MachineRegistry.RECYCLER_REGISTRY.block(Tier.MK2)))
-        add(FluidInfuserRecipe.IDENTIFIER, ItemStack(MachineRegistry.FLUID_INFUSER_REGISTRY.block(Tier.MK1)))
-        add(CondenserRecipe.IDENTIFIER, ItemStack(MachineRegistry.CONDENSER_REGISTRY.block(Tier.MK4)))
-        add(SmelterRecipe.IDENTIFIER, ItemStack(MachineRegistry.SMELTER_REGISTRY.block(Tier.MK4)))
-        add(SawmillRecipe.IDENTIFIER, ItemStack(MachineRegistry.SAWMILL_REGISTRY.block(Tier.MK4)))
-        add(ModuleRecipe.IDENTIFIER, ItemStack(MachineRegistry.MODULAR_WORKBENCH_REGISTRY.block(Tier.MK4)))
-        add(LaserRecipe.IDENTIFIER, ItemStack(MachineRegistry.LASER_EMITTER_REGISTRY.block(Tier.MK4)))
-        add(ElectrolysisRecipe.IDENTIFIER, ItemStack(MachineRegistry.ELECTROLYTIC_SEPARATOR_REGISTRY.block(Tier.MK4)))
-    }
-
-    private fun registerRecipes(registry: EmiRegistry) {
-        for (type in registry.recipeManager.recipes.keys) {
-            if (type is IRRecipeType<*> && type.id.namespace == IndustrialRevolution.MOD_ID) {
-                val category = categories[type.id] ?: continue
-                @Suppress("UNCHECKED_CAST")
-                val irType = type as IRRecipeType<IRRecipe>
-                for (recipe in registry.recipeManager.getAllOfType(irType).values) {
-                    if (recipe is IRRecipe) {
-                        registry.addRecipe(IRMachineEmiRecipe(recipe, category))
-                    }
-                }
+            for (tier in entry.registry.tiers) {
+                registry.addWorkstation(category, EmiStack.of(entry.registry.block(tier)))
             }
         }
     }
 
-    private fun registerWorkstations(registry: EmiRegistry) {
-        for ((id, machineReg) in recipeMachineMap) {
-            val category = categories[id] ?: continue
-            machineReg.forEachBlock { _, block ->
-                registry.addWorkstation(category, EmiStack.of(block))
+    @Suppress("UNCHECKED_CAST")
+    private fun registerRecipes(registry: EmiRegistry) {
+        for (entry in entries) {
+            val category = categories[entry.type.id] ?: continue
+            val type = entry.type as IRRecipeType<IRRecipe>
+            for (recipe in registry.recipeManager.getAllOfType(type).values) {
+                if (recipe is IRRecipe) {
+                    registry.addRecipe(IRMachineEmiRecipe(recipe, category))
+                }
             }
         }
     }
